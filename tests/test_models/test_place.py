7386e120f5192c3e37d4797a07a9f96e4a8a0a12
Raisa <r.irakoze@alustudent.com>
2023-10-07 21:58:47 +0100

    updated

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -15,55 +15,66 @@ class test_Place(test_basemodel):
 
     def test_city_id(self):
         """ """
-        new = self.value(city_id="fsdfsd-45454-4545-45454")
+        new = self.value()
+        new.city_id = "CA"
         self.assertEqual(type(new.city_id), str)
 
     def test_user_id(self):
         """ """
-        new = self.value(user_id="fsdfsd-4554-45654-fdfd")
+        new = self.value()
+        new.user_id = "CA"
         self.assertEqual(type(new.user_id), str)
 
     def test_name(self):
         """ """
-        new = self.value(name="hbo")
+        new = self.value()
+        new.name = "CA"
         self.assertEqual(type(new.name), str)
 
     def test_description(self):
         """ """
-        new = self.value(description="hey you")
+        new = self.value()
+        new.description = "CA"
         self.assertEqual(type(new.description), str)
 
     def test_number_rooms(self):
         """ """
-        new = self.value(number_rooms=6)
+        new = self.value()
+        new.number_rooms = 1
         self.assertEqual(type(new.number_rooms), int)
 
     def test_number_bathrooms(self):
         """ """
-        new = self.value(number_bathrooms=78)
+        new = self.value()
+        new.number_bathrooms = 1
         self.assertEqual(type(new.number_bathrooms), int)
 
     def test_max_guest(self):
         """ """
-        new = self.value(max_guest=8)
+        new = self.value()
+        new.max_guest = 1
         self.assertEqual(type(new.max_guest), int)
 
     def test_price_by_night(self):
         """ """
-        new = self.value(price_by_night=500)
+        new = self.value()
+        new.price_by_night = 1
         self.assertEqual(type(new.price_by_night), int)
 
     def test_latitude(self):
         """ """
-        new = self.value(latitude=0.5)
+        new = self.value()
+        new.latitude = 1.0
         self.assertEqual(type(new.latitude), float)
 
     def test_longitude(self):
         """ """
-        new = self.value(longitude=5.6)
+        new = self.value()
+        new.longitude = 3.0
         self.assertEqual(type(new.longitude), float)
 
     def test_amenity_ids(self):
         """ """
         new = self.value()
+        new.amenity_ids = ["CA"]
         self.assertEqual(type(new.amenity_ids), list)
